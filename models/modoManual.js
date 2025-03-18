c172143799514bb0d2c956c247e5f9d1918f7974
Luisander2651 <luisander2651@gmail.com>
2025-03-18 07:20:16 -0600

    se termino de hacer cada ruta

--- models/modoManual.js
+++ models/modoManual.js
@@ -2,17 +2,20 @@ const express = require("express");
 const router = express.Router();
 const connectDB = require("../db/conection");
 
-
 // GET movimientos_manuales solo el que tenga el esatdo activo en ese momento
 router.get("/", async (req, res) => {
   let connection;
   try {
     const pool = await connectDB();
     connection = await pool.getConnection();
-    const [rows] = await connection.execute("SELECT movimiento FROM movimientos_manuales where estado_movimiento = 'activado'");
+    const [rows] = await connection.execute(
+      "SELECT movimiento FROM movimientos_manuales where estado_movimiento = 'activado'"
+    );
 
     if (rows.length === 0) {
-      return res.status(404).json({ msg: "No se encontraron movimientos_manuales" });
+      return res
+        .status(404)
+        .json({ msg: "No se encontraron movimientos_manuales" });
     }
 
     res.json(rows);
@@ -49,34 +52,54 @@ router.get("/:id", async (req, res) => {
 });
 
 //put para cambiar el estado del movimiento a activado o desactivado
-router.put("/:id", async (req, res) => {
+router.put("/modo", async (req, res) => {
   let connection;
   try {
     //declaramos nuestra variables que necesitaremos para hacer las modificaciones correspondientes
-    const { id } = req.params;
-    const { estadoMovimiento } = req.body;
-
-    //validamos si los datos estan completos
-    if (!estadoMovimiento) {
-      return res.status(400).json({ msg: "Faltan datos" });
-    }
+    const { id } = req.body;
+    const estadoMovimiento = "activado"
 
     //conectamos a la base de datos y hacemos la consulta correspondiente
     const pool = await connectDB();
     connection = await pool.getConnection();
-    //Hacemos el cambio de activado a desactivado 
-    const query2 = "UPDATE movimientos_manuales SET estado_movimiento = ? WHERE estado_movimiento = ?";
-    //Guardamos el resultado en update para validar si hizo cambios o no
-    const [update] = await connection.query(query2, ["desactivado", "activado"]);
-    
-    //Validamos si se hizo el cambio de estado o no
-    if (update.affectedRows === 1) {
-      //Actualizamos el cambio de estado al siguiente movimiento despues de haber desactivado el cambio ya activo
-      const query = "UPDATE movimientos_manuales SET estado_movimiento = ? WHERE id_movimiento = ?";
-      const [result] = await connection.query(query, [estadoMovimiento, id]);
-      res.status(200).json({ msg: "Movimiento Actualizado", result });
+
+    //Actualizamos el cambio de estado al siguiente movimiento despues de haber desactivado el cambio ya activo
+    const query =
+      "UPDATE movimientos_manuales SET estado_movimiento = ? WHERE id_movimiento = ?";
+    const [result] = await connection.query(query, [estadoMovimiento, id]);
+    if (result.affectedRows === 1) {
+      return res.status(200).json({ msg: "Movimiento Actualizado", result });
     } else {
-      res.status(404).json({ msg: "Movimiento no encontrado" });
+      res.status(400).json({ msg: "Movimiento Actualizado", result });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Error al actualizar el movimiento" });
+  } finally {
+    if (connection) connection.release();
+  }
+});
+
+router.put("/status", async (req, res) => {
+  let connection;
+  try {
+    //declaramos nuestras variables que necesitaremos para hacer las modificaciones correspondientes
+    const estadoMovimiento = "desactivado";
+    const movActivate = "activado";
+
+    //conectamos a la base de datos y hacemos la consulta correspondiente
+    const pool = await connectDB();
+    connection = await pool.getConnection();
+
+    //Actualizamos el cambio de estado al siguiente movimiento despues de haber desactivado el cambio ya activo
+    const query =
+      "UPDATE movimientos_manuales SET estado_movimiento = ? WHERE estado_movimiento = ?";
+    const [result] = await connection.query(query, [estadoMovimiento, movActivate]);
+    
+    if (result.affectedRows === 1) {
+      return res.status(200).json({ msg: "Movimiento Actualizado", result });
+    } else {
+      res.status(400).json({ msg: "Movimiento Actualizado", result });
     }
   } catch (error) {
     console.error(error);
