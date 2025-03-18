5e9b9c2ad538f4fcec419166d3230467cd628545
Luisander2651 <luisander2651@gmail.com>
2025-03-18 12:23:03 -0600

    se cambio el status que devuelve la ruta

--- models/modoManual.js
+++ models/modoManual.js
@@ -16,7 +16,7 @@ const connectDB = require("../db/conection");
         return res.status(404).json({ msg: "No se encontraron movimientos" });
       }
       
-      res.status(500).json(rows);
+      res.status(200).json(rows);
     } catch (error) {
       res.status(500).json({ msg: "Error al conectar a la base de datos" });
     } finally {
