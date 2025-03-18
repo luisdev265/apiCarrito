922086a164c6318067ccfe9a6ed24ae92689f9f6
Luisander2651 <luisander2651@gmail.com>
2025-03-18 07:52:40 -0600

    se agrego una ultima ruta para solicitar el ultimo movimiento subido en la tabla

--- models/modoManual.js
+++ models/modoManual.js
@@ -2,6 +2,28 @@ const express = require("express");
 const router = express.Router();
 const connectDB = require("../db/conection");
 
+//GET Ultimo movimiento
+router.get("/ultimo", async (req, res) => {
+  let connection;
+  try {
+    const pool = await connectDB();
+    connection = await pool.getConnection();
+    const [rows] = await connection.execute(
+      "SELECT movimiento FROM movimientos ORDER BY id DESC LIMIT 1"
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ msg: "No se encontraron movimientos" });
+    }
+    
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ msg: "Error al conectar a la base de datos" });
+  } finally {
+    if (connection) connection.release();
+  }
+});
+
 // GET movimientos_manuales solo el que tenga el esatdo activo en ese momento
 router.get("/", async (req, res) => {
   let connection;
@@ -95,7 +117,7 @@ router.put("/status", async (req, res) => {
     const query =
       "UPDATE movimientos_manuales SET estado_movimiento = ? WHERE estado_movimiento = ?";
     const [result] = await connection.query(query, [estadoMovimiento, movActivate]);
-    
+
     if (result.affectedRows === 1) {
       return res.status(200).json({ msg: "Movimiento Actualizado", result });
     } else {
