443a2a8221bd038d7213a9e33239a694e0f7c29b
Luisander2651 <luisander2651@gmail.com>
2025-03-21 04:42:23 -0600

    se agrego un endpoint mas a movimientos para eliminar los registros cada que se cambie el modo del carrito

--- models/movimientos.js
+++ models/movimientos.js
@@ -3,48 +3,72 @@ const router = express.Router();
 const connectDB = require("../db/conection");
 
 router.post("/", async (req, res) => {
-    try {
-  
-      const {movimiento} = req.body;
-  
-      if (!movimiento) {
-        return res.status(400).json({ msg: "Faltan datos" });
-      }
-  
-      const conection = await connectDB();
-      const query = "INSERT INTO movimientos (movimiento) VALUES (?)";
-  
-      await conection.query(query, [movimiento]);
-  
-      res.status(201).json({ msg: "Movimiento registrado" });
-  
-    }catch (error) {
-      res.status(500).json({ msg: "Error al conectar a la base de datos", ERROR: error.message });
-  
+  try {
+    const { movimiento } = req.body;
+
+    if (!movimiento) {
+      return res.status(400).json({ msg: "Faltan datos" });
     }
-  });
 
-  router.get("/", async (req, res) => {
-    try {
-      const conection = await connectDB();
-      const [rows] = await conection.execute("SELECT id, DATE_FORMAT(fechaYhora, '%Y-%m-%d') AS fecha, TIME_FORMAT(fechaYhora, '%H:%i:%s') AS hora, movimiento FROM movimientos");
-      res.json(rows);
-    } catch (error) {
-      res.status(500).json({ msg: "Error al conectar a la base de datos" });
+    const conection = await connectDB();
+    const query = "INSERT INTO movimientos (movimiento) VALUES (?)";
+
+    await conection.query(query, [movimiento]);
+
+    res.status(201).json({ msg: "Movimiento registrado" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        msg: "Error al conectar a la base de datos",
+        ERROR: error.message,
+      });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const conection = await connectDB();
+    const [rows] = await conection.execute(
+      "SELECT id, DATE_FORMAT(fechaYhora, '%Y-%m-%d') AS fecha, TIME_FORMAT(fechaYhora, '%H:%i:%s') AS hora, movimiento FROM movimientos"
+    );
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ msg: "Error al conectar a la base de datos" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const conection = await connectDB();
+    const [rows] = await conection.execute(
+      "SELECT id, DATE_FORMAT(fechaYhora, '%Y-%m-%d') AS fecha, TIME_FORMAT(fechaYhora, '%H:%i:%s') AS hora, movimiento FROM movimientos WHERE id = ?",
+      [id]
+    );
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ msg: "Error al conectar a la base de datos" });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    
+    const conection = await connectDB();
+    const query = "DELETE FROM movimientos";
+    const query2 = "ALTER TABLE movimientos AUTO_INCREMENT = 1"
+    const [ rows ] = await conection.query(query);
+    await conection.query(query2);
+
+    if (rows.affectedRows > 0) {
+      return res.status(200).json({ msg: "Base de datos limpiada con exito" });
     }
-  });
 
-  router.get("/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const conection = await connectDB();
-      const [rows] = await conection.execute( "SELECT id, DATE_FORMAT(fechaYhora, '%Y-%m-%d') AS fecha, TIME_FORMAT(fechaYhora, '%H:%i:%s') AS hora, movimiento FROM movimientos WHERE id = ?", [id]);
-      res.json(rows);
-    } catch (error) {
-      res.status(500).json({ msg: "Error al conectar a la base de datos" });
-    }
-  });
+    res.json({ msg: "Movimiento eliminado" });
+  } catch (error) {
+    res.status(500).json({ msg: "Error al conectar a la base de datos" });
+  }
+});
 
-
-
-  module.exports = router;
+module.exports = router;
