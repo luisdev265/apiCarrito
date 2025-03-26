8a492a1399ba35db3e90b6451515f104c49f6d1b
luisdev265 <luisander.dev@gmail.com>
2025-03-25 22:08:46 -0600

    sE MODIFICARON LAS RUTAS DE ACTUALIZACION DE DISTANCIA PARA MEJORAR LA RESPUESTA DEL SERVIDOR Y GESTIONAR MEJOR EN APP aNDROID

--- models/distancia.js
+++ models/distancia.js
@@ -5,7 +5,7 @@ const connectDB = require("../db/conection");
 router.get("/", async (req, res) => {
     try {
         const conection = await connectDB();
-        const query = "SELECT * FROM distancia";
+        const query = "SELECT distancia FROM distancia";
         const [rows] = await conection.query(query);
 
         if (rows.length === 0) {
@@ -31,11 +31,15 @@ router.put('/', async (req, res) => {
         const query = 'UPDATE distancia SET distancia =? WHERE id_distancia =?';
         const [result] = await conection.query(query, [distancia, id]);
 
-        if (result.affectedRows === 0) {
-            return res.status(404).json({ msg: 'No se pudo actualizar la distancia' });
-        }
+        if (result.affectedRows > 0) {
 
-        res.status(200).json({ msg: 'Distancia actualizada correctamente' });
+            const query2 = 'select * from distancia where id_distancia =?'
+            const [rows] = await conection.query(query2, id);
+
+            return res.status(200).json({ msg: 'Distancia actualizada correctamente', distancia: rows });
+        }
+        
+        return res.status(404).json({ msg: 'No se pudo actualizar la distancia' });
     } catch (error) {
         res.status(500).json({ msg: 'Error al actualizar la distancia', error: error.message }); 
     } 
--- models/velocidad.js
+++ models/velocidad.js
@@ -5,7 +5,7 @@ const connectDB = require('../db/conection');
 router.get('/', async (req, res) => {
   try {
     const conection = await connectDB();
-    const query = 'SELECT * FROM velocidad';
+    const query = 'SELECT velocidad FROM velocidad';
     const [rows] = await conection.query(query);
 
     if (rows.length === 0) {
@@ -31,11 +31,15 @@ router.put('/', async (req, res) => {
     const query = 'UPDATE velocidad SET velocidad = ? WHERE id_velocidad = ?';
     const [result] = await conection.query(query, [velocidad, id]);
 
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ msg: 'No se pudo actualizar la velocidad' });
-    }
+    if (result.affectedRows > 0) {
 
-    res.status(201).json({ msg: 'Velocidad actualizada' });
+      const query2 = 'SELECT * FROM velocidad WHERE id_velocidad = ?';
+      const [rows] = await conection.query(query2, id);
+      
+      return res.status(201).json({ msg: 'Velocidad actualizada', velocidad: rows });
+    }
+    
+    return res.status(404).json({ msg: 'No se pudo actualizar la velocidad' });
   } catch (error) {
     res.status(500).json({ msg: 'Error al actualizar la velocidad', error: error.message });
   }
